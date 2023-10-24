c44f4d02cefa6d552a4bfccc951028ebba2f8c19
youngdev57 <youngdev57@gmail.com>
2023-10-24 15:50:53 +0900

    Update config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   outputDir: './docs',
-  publicPath: process.env.NODE_ENV === 'production' ? '/<repository name>/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/javascript-archive/' : '/',
 }
